44fc6ab5c7c8c6b894ebcde7b5414c2b860035f3
Neil Johnson <realdealneil@gmail.com>
2019-03-11 03:08:32 -0600

    Fixed a few things to allow passing a path into the application

--- submission/rdr_generate_submission.py
+++ submission/rdr_generate_submission.py
@@ -1,6 +1,7 @@
 # Load libraries
 import json
 from pprint import pprint
+import argparse
 import glob
 import cv2
 import numpy as np
@@ -9,8 +10,18 @@ from random import shuffle
 from generate_results import *
 import time
 
+# Load a file, do yolo detections on it:
+ap = argparse.ArgumentParser()
+ap.add_argument('-p','--path', help='path to folder with images in it')
+args = ap.parse_args()
 
-img_file = glob.glob('../testing/images/*.JPG')
+path = '../testing/images/'
+
+if args.path is not None:
+    path = args.path
+    print("Path: %s" % path)
+
+img_file = glob.glob(path + '/*.JPG')
 img_keys = [img_i.split('/')[-1] for img_i in img_file]
 
 # Testing individual corner cases:
@@ -35,10 +46,10 @@ finalDetector = GenerateFinalDetections()
 time_all = []
 pred_dict = {}
 c = 0
-saveImages = 1
-output_path = "../images_out/"
+saveImages = 0
+#output_path = "../images_out/"
 for img_key in img_keys:
-	img =cv2.imread('../testing/images/'+img_key)
+	img =cv2.imread(path + '/' + img_key)
 	print("Image %s: %s" % (c, img_key))
 	#img =cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 	tic = time.monotonic()
@@ -50,8 +61,8 @@ for img_key in img_keys:
 	c = c + 1
 
 	cv2.imshow("yolo detection", img)
-	if saveImages:
-		cv2.imwrite(output_path + "out_" + img_key, img)
+	#if saveImages:
+	#	cv2.imwrite(output_path + "out_" + img_key, img)
 	if (finalDetector.inspect):
 		#key = cv2.waitKey(0)
 		key = cv2.waitKey(15)
